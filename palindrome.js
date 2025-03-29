d47bf1711a895708ce1ac1a961e5c16a980d609a
Jesus T. <jesustellor@gmail.com>
2025-03-29 02:35:46 +0000

    check if word is spelled the same backwords

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,15 @@
 function palindrome(str){
     let rev = "";
+    // let rev = str.split("").reverse().join("");
     for(let i = 0;  i < str.length; i++){
         rev = str[i] + rev;
     }
     return rev === str;
+    //if(rev === str){
+    //    return true;
+    //}else{
+    //    return false;
+   // }
 }
 
 console.log(palindrome("racecar"));
