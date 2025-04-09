7814e5f434a4a23fc56f56cbab7018d5dd9689f7
Jesus T. <jesustellor@gmail.com>
2025-04-09 22:05:06 +0000

    two different ways to checking 2 string for anagram

--- anagrams.js
+++ anagrams.js
@@ -1,23 +1,31 @@
-function charMap(str){
-    const charMap = {};
-    str = str.toLowerCase().replace(/[\W]/g, '');
-
-    for (let char of str){
-        charMap[char] = (charMap[char] || 0) + 1;
-    }
-    return charMap;
+function cleanStr(str) {
+    return str.toLowerCase().replace(/[\W]/g, '').split('').sort().join('');
+}
+function anagrams(str1, str2) {
+    return cleanStr(str1) === cleanStr(str2);
 }
 
-function anagrams(str1, str2){
-    const charMap1 = charMap(str1);
-    const charMap2 = charMap(str2);
-    for (let key in charMap1){
-        console.log(charMap2[key]);
-        if (charMap1[key] !== charMap2[key]){
-            return false;
-        }
-    }
-    return true;
-}
-// Test the function
-console.log(anagrams("listen", "inlet")); // ["inlets", "enlist"]
+
+// function charMap(str){
+//     const charMap = {};
+//     str = str.toLowerCase().replace(/[\W]/g, '');
+
+//     for (let char of str){
+//         charMap[char] = (charMap[char] || 0) + 1;
+//     }
+//     return charMap;
+// }
+
+// function anagrams(str1, str2){
+//     const charMap1 = charMap(str1);
+//     const charMap2 = charMap(str2);
+//     for (let key in charMap1){
+//         console.log(charMap2[key]);
+//         if (charMap1[key] !== charMap2[key]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// // Test the function
+console.log(anagrams("listen", "inlets")); // ["inlets", "enlist"]
