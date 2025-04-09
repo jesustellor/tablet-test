3ba920dfc6b9d88c33fe36a4c2677ef68bfa473c
Jesus T. <jesustellor@gmail.com>
2025-04-09 04:56:38 +0000

    passing in a word and returning froma  list of words the anagrams

--- anagrams.js
+++ anagrams.js
@@ -1,3 +1,12 @@
-function anagrams(){
-    
+function anagrams(word){
+    const anagrams = ["enlist", "google", "inlets", "banana"];
+
+    const result = anagrams.filter(anagram => {
+        return anagram.split("").sort().join("") === word.split("").sort().join("");
+    });
+
+    console.log(result); // ["inlets"]
+    return result; 
 }
+// Test the function
+anagrams("listen"); // ["inlets", "enlist"]
