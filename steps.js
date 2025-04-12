0434da1accecd9a0423dee17d3a4946e167e0cd4
Jesus T. <jesustellor@gmail.com>
2025-04-12 10:36:11 +0000

    wrapping my head around double loops

--- steps.js
+++ steps.js
@@ -1,16 +1,40 @@
-function steps(n) {
-    for(let row=1; row<=n; row++){
-     let line = ''
-     for(let col=2; col<=n; col++){
-       if(col<=row){
-         line += '#'
-       }else{
-         line += ' '
-       }
-     }
-     console.log(line)
-    } 
-   }
+function loops(n) {
+    for (let i = 0; i < n; i++) {
+        console.log('i:', i);
+        for (let j = 0; j < n; j++) {
+            console.log('j:', j);
+            // console.log(i, j);   
+        }
+    }
+}
+
+// function steps(n) {
+//     for (let i = 0; i < n; i++){
+//         let line = '';
+//         for (let j = 0; j < n; j++){
+//             if (j > i) {
+//                 line += ' ';
+//             } else {
+//                 line += '#';
+//             }
+//         }
+//         console.log(line);
+//     }
+// }
+
+// function steps(n) {
+//     for(let row=1; row<=n; row++){
+//      let line = ''
+//      for(let col=2; col<=n; col++){
+//        if(col<=row){
+//          line += '#'
+//        }else{
+//          line += ' '
+//        }
+//      }
+//      console.log(line)
+//     } 
+//    }
 
 // function steps(n){
 //     for(let i = 1; i <= n; i++){
@@ -18,4 +42,4 @@ function steps(n) {
 //     }
 // }
 // Test the function
-steps(3);
+loops(3);
