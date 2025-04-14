ede2b10248d8674aee9a2638275295687a76fc31
Jesus T. <jesustellor@gmail.com>
2025-04-14 04:56:43 +0000

    movingg on to pyramid

--- steps.js
+++ steps.js
@@ -1,29 +1,30 @@
-function loops(n) {
-    for (let ro = 0; ro < n; ro++) {
-        // console.log('row:', ro);
-        let row = '';
-        for (let col = 0; col < n; col++) {
-            // console.log('col:', col);
-            row += '#';
-            // console.log(i, j);   
+// function steps(n) {
+//  for(let row = 1; row <= n; row++){
+//      let line = '';
+//      for(let col = 1; col <= n; col++){
+//          if(col <= row){
+//              line += '#';
+//          } else {
+//              line += '  ~';
+//          }
+//      }
+//      console.log(line);
+//  }
+// }
+
+function steps(n) {
+    for (let i = 0; i < n; i++){
+        let line = '';
+        for (let j = 0; j < n; j++){
+            if (j > i) {
+                line += ' ';
+            } else {
+                line += '#';
+            }
         }
-        console.log(row);
+        console.log(line);
     }
 }
-
-// function steps(n) {
-//     for (let i = 0; i < n; i++){
-//         let line = '';
-//         for (let j = 0; j < n; j++){
-//             if (j > i) {
-//                 line += ' ';
-//             } else {
-//                 line += '#';
-//             }
-//         }
-//         console.log(line);
-//     }
-// }
 
 // function steps(n) {
 //     for(let row=1; row<=n; row++){
@@ -45,4 +46,4 @@ function loops(n) {
 //     }
 // }
 // Test the function
-loops(3);
+steps(3);
