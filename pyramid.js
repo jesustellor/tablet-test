3aed8a50f2e0c7450a6b941d136b080527fcb313
Jesus T. <jesustellor@gmail.com>
2025-04-15 04:19:11 +0000

    using mid var for formula

--- pyramid.js
+++ pyramid.js
@@ -1,22 +1,11 @@
-// function pyramid(n) {
-//     for(let row = 1; row <= n; row++){
-//         let line = '';
-//         for(let col = 1; col <= n; col++){
-//             if(col <= row){
-//                 line += '#';
-//             } else {
-//                 line += ' ';
-//             }
-//         }
-//         console.log(line);
-//     }
-// }
-
+// 2n - 1 columns and math.floor(2n - 1 / 2) where to place the #
+// col >= mid - row && col <= mid + row
 function pyramid(n) {
     for(let row = 0; row < n; row++){
         let line = '';
-        for(let col = 0; col < 2 * n -1; col++){
-            if(col >= n - row - 1 && col <= n + row - 1){
+        let mid = Math.floor((2 * n - 1) / 2);
+        for(let col = 0; col < n * 2 - 1; col++){
+            if(col >= mid - row && col <= mid + row){
                 line += '#';
             } else {
                 line += ' ';
@@ -26,5 +15,19 @@ function pyramid(n) {
     }
 }
 
-// Test the function
-pyramid(3);
+// function pyramid(n) {
+//     for(let row = 0; row < n; row++){
+//         let line = '';
+//         for(let col = 0; col < 2 * n -1; col++){
+//             if(col >= n - row - 1 && col <= n + row - 1){
+//                 line += '#';
+//             } else {
+//                 line += ' ';
+//             }
+//         }
+//         console.log(line);
+//     }
+// }
+
+// // Test the function
+pyramid(2);
